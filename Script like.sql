bc3efb407e85e7816641eda0c62f1622432b720c
gabrielgoncalves17 <gabrielmv@hotmail.com.br>
2017-07-01 19:10:57 -0300

    Add files via upload

---
+++ Script like.sql
@@ -0,0 +1,3 @@
+select * from operador where datanasc_operador like '199%-%-2%';
+select * from operador where rua_operador like 'rua n%';
+select * from tipo_operador where descricao_tipo_operador like 'lider';
