36770876a729c27b43d680a5abb24d3412241d9f
gabrielgoncalves17 <gabrielmv@hotmail.com.br>
2017-07-01 19:45:08 -0300

    Add files via upload

---
+++ Script update.sql
@@ -0,0 +1,7 @@
+update estado set descricao_estado = 'ES' where id_estado = 10062;
+update operador set nome_operador = 'Carlos Santos' where id_operador = 10004;
+update tipo_operador set descricao_tipo_operador = 'Visitante' where id_tipo_operador = 10021;
+update tipo_ocorrencia set valor_tipo_ocorrencia = 99500 where id_evento_FK = 10081;
+update tipo_clima set descricao_tipo_clima = 'Tropical' where id_clima = 10151;
+update cidade set descricao_cidade = 'Vitória' where id_cidade = 10042;
+
